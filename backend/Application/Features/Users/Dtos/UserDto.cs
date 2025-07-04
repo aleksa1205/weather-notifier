7c084a03454d871893f220ac5687cb50606cf7ca
Aleksa Peric <alex.peric1205@gmail.com>
2025-07-04 16:22:34 +0200

    Added query for user by id

---
+++ backend/Application/Features/Users/Dtos/UserDto.cs
@@ -0,0 +1,2 @@
+﻿namespace Application.Features.Users.Dtos;
+public record UserDto(Guid id, string email, string city);
---
+++ backend/Application/Features/Users/Queries/ById/GetUserByIdQuery.cs
@@ -0,0 +1,5 @@
+﻿using Application.Features.Users.Dtos;
+using MediatR;
+
+namespace Application.Features.Users.Queries.ById;
+public record GetUserByIdQuery(Guid Id) : IRequest<UserDto>;
